d448c215d2e096b80ac11b8d97c3f34513ec5ff0
Korbin de Man <113640462+korbindeman@users.noreply.github.com>
2022-10-19 22:22:22 +0200

    chore: pyright ignore false errors

--- app/db.py
+++ app/db.py
@@ -14,11 +14,10 @@ engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
 async def init_db():
     async with engine.begin() as conn:
-        # await conn.run_sync(SQLModel.metadata.drop_all)
         await conn.run_sync(SQLModel.metadata.create_all)
 
 
-async def get_session() -> AsyncSession:
-    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async def get_session() -> AsyncSession: # type: ignore
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False) # type: ignore
     async with async_session() as session:
         yield session
