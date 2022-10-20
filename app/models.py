e8c50d766c96d5e5dba2242eecc14037f93fdb3f
StinkyVoet <stinkyvoet@gmail.com>
2022-10-20 11:35:43 +0200

    feat: insert and retrieve nodes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+import random
+import string
 from sqlmodel import Field, SQLModel
 
 
@@ -8,3 +10,14 @@ class SensorData(SQLModel, table=True):
     temperature: float
     humidity: float
     pressure: float
+
+
+class NodeData(SQLModel, table=True):
+    def generateKey():
+        return "".join(random.choices(string.ascii_letters + string.digits, k=15))
+
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    description: str | None
+    slug: str | None = Field(unique=True)
+    # key: str | None = Field(default=generateKey, unique=True)
