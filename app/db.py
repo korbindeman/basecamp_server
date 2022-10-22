9e37180f914aafdfacb87c547a1d245ecc6eac5b
Korbin de Man <113640462+korbindeman@users.noreply.github.com>
2022-10-22 15:03:00 +0200

    build: use alembic for migrations

--- app/db.py
+++ app/db.py
@@ -1,8 +1,6 @@
 import os
 from dotenv import load_dotenv
 
-from sqlmodel import SQLModel
-
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -12,12 +10,7 @@ DATABASE_URL = os.environ["DATABASE_URL"]
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
 
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
-
-
-async def get_session() -> AsyncSession: # type: ignore
-    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False) # type: ignore
+async def get_session() -> AsyncSession:  # type: ignore
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)  # type: ignore
     async with async_session() as session:
         yield session
